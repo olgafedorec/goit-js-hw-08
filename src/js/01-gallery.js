d8d472c0d8e8e5146c0a7e2a749c98c226424438
olgafedorec <olga_fedorec@yahoo.com>
2023-05-01 23:25:41 +0300

    update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from './simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(item => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+</li>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
