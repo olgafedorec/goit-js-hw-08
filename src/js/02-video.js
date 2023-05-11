7ae4f1f140dfc7f381383ff6636fbcf7f68d8315
olgafedorec <olga_fedorec@yahoo.com>
2023-05-11 11:17:19 +0300

    update js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(LOCAL_KEY, seconds);
+  console.log(seconds);
+}
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash/throttle';
 
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  inputMale: document.querySelector('input[type="email"]'),
+  textMessage: document.querySelector('textarea[name="message"]'),
+  feedbackBtn: document.querySelector('button[type="submit"]'),
+};
+
+console.dir(refs.feedbackForm);
+
+const LOCAL_KEY = 'feedback-form-state';
+
+const feedback = {};
+
+refs.feedbackForm.addEventListner();
+
+function onSaveFeedback(event) {}
+
+// // feedbackForm.elements.email;
+// // feedbackForm.elements.message;
+
+// feedbackBtn.addEventListener('submit', clearInput());
